94b216e85da3f00a7ff47f55ff41fef54015b257
dasithsv <dasithsv@gmail.com>
2021-10-17 22:58:47 +0530

    Added a calculator in c

---
+++ calculator-c/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/*
+gcc -o calculator main.c - compile instructions 
+ELF 64-bit LSB pie executable, x86-64 - bianry file 
+*/
+
+int main()
+{
+    char op;
+    double first, second;
+    printf("Enter an operator (+, -, *, /): ");
+    scanf("%c", &op);
+    printf("Enter two operands: ");
+    scanf("%lf %lf", &first, &second);
+
+    switch (op)
+    {
+    case '+':
+        printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
+        break;
+    case '-':
+        printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
+        break;
+    case '*':
+        printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
+        break;
+    case '/':
+        printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+        break;
+    // operator doesn't match any case constant
+    default:
+        printf("Error! operator is not correct");
+    }
+
+    return 0;
+}
